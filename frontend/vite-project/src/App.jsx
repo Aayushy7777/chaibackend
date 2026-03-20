a816c0b9b06f27f7abdb636cdef789fc5e62c2e1
Aayushy7777 <aayu.sh7021@gmail.com>
2026-03-20 15:10:52 +0530

    integrating frontend & backend

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './App.css'
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+    .then((response) => {
+      setJokes(response.data);
+    }
+    )
+    .catch((error) => {
+      console.log(error); 
+    }
+    )
+  }, [jokes])
+
+  return (
+    //center the content
+
+<>
+     <link href="/src/style.css" rel="stylesheet">
+     </link>
+    
+    <h1> chai and full stack</h1>
+    <p> JOKES: {jokes.length}</p>
+
+
+
+    {
+          jokes.map((joke, index) => (
+       (
+          <div key = {joke.id}>   
+             <h3>{joke.title}</h3>   
+            <p>{joke.content}</p>
+
+          </div>
+        )
+      ))
+      
+    }
+     
+</> 
+
+
+  )
+}
+
+export default App
